de465c3dd5eac0eeb91949227004deb876ff6551
Aleksi O. Martikainen <aleksi.o.martikainen@student.tut.fi>
2019-11-15 23:00:41 +0200

    Add script to delete kids when entering (life)school
    Changes to all over the scene

---
+++ Keppi&Porkkana/Assets/Scripts/LifeschoolLogic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeschoolLogic : MonoBehaviour
+{
+    //Detect collisions between the GameObjects with Colliders attached
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "Child")
+        {
+            destroy(collision.gameObject);
+            RemoveMoney();
+        }
+    }
+    private void RemoveMoney() {
+        // TODO Remove money
+    }
+}

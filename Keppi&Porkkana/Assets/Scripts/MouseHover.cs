646125e0bbc6acec37fcea3d90e289d98828c215
Lauri Viitanen <lauri_viitanen@hotmail.com>
2019-11-15 22:22:28 +0200

    fixed filenames for manu scripts

---
+++ Keppi&Porkkana/Assets/Scripts/MouseHover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour {
+
+    // Start is called before the first frame update
+void Start() => renderer.material.color = Color.white;
+
+void OnMouseEnter() => Renderer.material.color = Color.yellow;
+
+void OnMouseExit() => Renderer.material.color = Color.white;
+
+}

6c90fdaaec5da23447e75a68e0651dfaffc52c57
Aleksi O. Martikainen <aleksi.o.martikainen@student.tut.fi>
2019-11-16 16:13:20 +0200

    Minor fixes for stick, it no longer collides with school

--- Keppi&Porkkana/Assets/Scripts/LifeschoolLogic.cs
+++ Keppi&Porkkana/Assets/Scripts/LifeschoolLogic.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LifeschoolLogic : MonoBehaviour
 {
     //Detect collisions between the GameObjects with Colliders attached
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider collision)
     {
         //Check for a match with the specific tag on any GameObject that collides with your GameObject
         if (collision.gameObject.tag == "Child")
--- Keppi&Porkkana/Assets/Scripts/SchoolLogic.cs
+++ Keppi&Porkkana/Assets/Scripts/SchoolLogic.cs
@@ -10,7 +10,7 @@ public class SchoolLogic : MonoBehaviour
     }
     
     //Detect collisions between the GameObjects with Colliders attached
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider collision)
     {
         //Check for a match with the specific tag on any GameObject that collides with your GameObject
         if (collision.gameObject.tag == "Child")

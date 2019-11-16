a0a24f851dfc89fcb700126ac3d4bc06a95bf028
ValtteriK <wr1337@protonmail.ch>
2019-11-16 09:53:01 +0200

    Added score texts and crude update logic

---
+++ Keppi&Porkkana/Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ScoreCounter : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Text scoreAmount;
+    private int frames;
+    void Start()
+    {
+        scoreAmount = GetComponent<Text>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frames++;
+        if(frames % 10 == 0)
+        {
+            int scoreValue = Int32.Parse(scoreAmount.text);
+            scoreAmount.text = (scoreValue + 1).ToString();
+        }
+    }
+}

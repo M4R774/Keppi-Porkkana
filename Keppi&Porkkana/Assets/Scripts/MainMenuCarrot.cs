f750847055c3181f5f2ac5790ee9f059cf1303ca
Lauri Viitanen <lauri_viitanen@hotmail.com>
2019-11-16 14:07:40 +0200

    MainMenuFixed

--- Keppi&Porkkana/Assets/Scripts/MainMenuCarrot.cs
+++ Keppi&Porkkana/Assets/Scripts/MainMenuCarrot.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class MainMenuCarrot : MonoBehaviour
 {
 
-    public float distanceY = -75f;
+    public float distanceY = 0;
+    public float distanceZ = 40;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,8 @@ public class MainMenuCarrot : MonoBehaviour
     void Update()
     {
         Vector3 mousePosition = Input.mousePosition;
-        mousePosition.y = distanceY;
+        mousePosition.y += distanceY;
+        mousePosition.z += distanceZ;
         transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
     }
 }
